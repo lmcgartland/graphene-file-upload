8d0d60d21dc65b149ee5c96e9e211d09bf3e8e12
Luke McGartland <mcgartla@usc.edu>
2018-04-05 00:15:42 -0700

    Updated description

--- setup.py
+++ setup.py
@@ -20,13 +20,13 @@ setup(
   name = 'graphene_file_upload',
   packages = ['graphene_file_upload'], # this must be the same as the name above
   version = '0.1.0',
-  description = 'Lib for adding file upload functionality to Graphene Django',
+  description = 'Lib for adding file upload functionality to GraphQL mutations in Graphene Django',
   long_description=long_description,
   long_description_content_type='text/x-rst',
   author = 'Lucas McGartland',
   author_email = 'luke@thebeeinc.com',
   url = 'https://github.com/lmcgartland/graphene-file-upload', # use the URL to the github repo
-  download_url = 'https://github.com/lmcgartland/graphene-file-upload/archive/0.1.0.tar.gz', # I'll explain this in a second
+  # download_url = 'https://github.com/lmcgartland/graphene-file-upload/archive/0.1.0.tar.gz',
   keywords = ['graphql', 'graphene', 'apollo',  'upload'], # arbitrary keywords
   classifiers = [],
   # install_requires=[]
