d96062aa511a2ca567fec398b77529ac97b0d0f5
davidroeca <david.roeca@gmail.com>
2018-07-06 15:50:19 -0400

    fix data undefined error

--- graphene_file_upload/django.py
+++ graphene_file_upload/django.py
@@ -9,8 +9,8 @@ class ModifiedGraphQLView(GraphQLView):
     def parse_body(self, request):
         content_type = self.get_content_type(request)
         if content_type == 'multipart/form-data':
-            operations = json.loads(data.get('operations', '{}'))
-            files_map = json.loads(data.get('map', '{}'))
+            operations = json.loads(request.POST.get('operations', '{}'))
+            files_map = json.loads(request.POST.get('map', '{}'))
             return place_files_in_operations(
                 operations,
                 files_map,
