45881396549e6e960afe48deaa674dc85dcc14a6
Cameron Hurst <cahurst@cisco.com>
2021-01-06 13:57:55 -0500

    feat: support version 3 for graphene

--- graphene_file_upload/flask/__init__.py
+++ graphene_file_upload/flask/__init__.py
@@ -1,6 +1,6 @@
 """Apply multipart request spec to flask"""
 from flask import request
-from flask_graphql import GraphQLView
+from graphql_server.flask import GraphQLView
 from graphql_server import load_json_body
 
 from ..utils import place_files_in_operations
@@ -12,12 +12,8 @@ class FileUploadGraphQLView(GraphQLView):
     def parse_body(self):
         """Handle multipart request spec for multipart/form-data"""
         content_type = request.mimetype
-        if content_type == 'multipart/form-data':
-            operations = load_json_body(request.form.get('operations', '{}'))
-            files_map = load_json_body(request.form.get('map', '{}'))
-            return place_files_in_operations(
-                operations,
-                files_map,
-                request.files
-            )
+        if content_type == "multipart/form-data":
+            operations = load_json_body(request.form.get("operations", "{}"))
+            files_map = load_json_body(request.form.get("map", "{}"))
+            return place_files_in_operations(operations, files_map, request.files)
         return super(FileUploadGraphQLView, self).parse_body()
