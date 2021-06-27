f33d2257591dce55dea9c315eea1f7fdc3a56f9a
Tonye Jack <jtonye@ymail.com>
2021-06-27 16:32:01 -0400

    Add Django 3.2 support

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.0",
         "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
         "Framework :: Flask"
     ],
     extras_require={
