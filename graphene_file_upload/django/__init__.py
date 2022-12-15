fe70738a7e50cfb4ebfb90fdd8959213cbfe73fd
Aprotim Biswas <81891553+Apro2011@users.noreply.github.com>
2022-12-15 20:34:28 +0530

    Fixed json loads error

--- graphene_file_upload/django/__init__.py
+++ graphene_file_upload/django/__init__.py
@@ -12,8 +12,8 @@ class FileUploadGraphQLView(GraphQLView):
         """Handle multipart request spec for multipart/form-data"""
         content_type = self.get_content_type(request)
         if content_type == 'multipart/form-data' and 'operations' in request.POST:
-            operations = json.loads(request.POST.get('operations', '{}'))
-            files_map = json.loads(request.POST.get('map', '{}'))
+            operations = json.loads(request.POST.get('operations', '{}'), strict=False)
+            files_map = json.loads(request.POST.get('map', '{}'), strict=False)
             return place_files_in_operations(
                 operations,
                 files_map,
